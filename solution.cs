3f50094b108448aeec08b41383135d55baadd210
Kooreis <kooreis.official@gmail.com>
2023-10-21 23:20:18 -0500

    This update adds the DFSUtil method, which is a helper function for the Depth-First Search. It marks the current vertex as visited, adds it to the path, and then recursively visits all unvisited adjacent vertices. If a cycle is detected, it checks if it's the longest cycle found so far.

--- solution.cs
+++ solution.cs
@@ -1,10 +1,19 @@
-class Graph {
-    constructor(vertices) {
-        this.vertices = vertices;
-        this.adj = new Array(vertices).fill(null).map(() => []);
-    }
+DFSUtil(v, visited, path) {
+        visited[v] = true;
+        path.push(v);
 
-    addEdge(u, v) {
-        this.adj[u].push(v);
+        for (let i of this.adj[v]) {
+            if (!visited[i]) {
+                this.DFSUtil(i, visited, path);
+            } else if (path.includes(i)) {
+                let cycleStart = path.indexOf(i);
+                let cycleLength = path.length - cycleStart;
+                if (cycleLength > this.longestCycle.length) {
+                    this.longestCycle = path.slice(cycleStart);
+                }
+            }
+        }
+
+        path.pop();
+        visited[v] = false;
     }
-}
